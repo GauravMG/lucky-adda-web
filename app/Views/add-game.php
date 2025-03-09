df231ea1b4d799533075216868dd231b935b3964
GauravMG <gauravmittal17396@gmail.com>
2025-03-09 15:04:26 +0530

    game edit option

--- app/Views/add-game.php
+++ app/Views/add-game.php
@@ -71,7 +71,9 @@
     const gameId = Number('<?php echo $data["gameId"]; ?>')
 
     document.addEventListener("DOMContentLoaded", function() {
-        fetchGame()
+        if (gameId) {
+            fetchGame()
+        }
 
         function isValidTimeFormat(time) {
             return /^([01]\d|2[0-3]):([0-5]\d)$/.test(time); // Ensures HH:MM format
